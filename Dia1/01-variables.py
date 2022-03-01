4c006d5b79163d439a198668236977405455dae6
Juan Diego <jdalbinoramos@gmail.com>
2022-02-28 21:31:19 -0500

    semana01

---
+++ Dia1/01-variables.py
@@ -0,0 +1,32 @@
+#Esto es un comentario y sirve para dar contexto de que se hace, se hizo o se hará.
+#TODO: logica para este controlador.
+
+nombre = 'eduardo'
+edad = 30
+
+#variables de texto
+nombre = 'eduardo'
+apellido = "de'lala"
+descripcion = """hola amigos:
+como estan?"""
+
+descripcion2 = '''Hola amigos:
+what is up?'''
+
+print(nombre)
+
+#variables numericas
+
+year = 2022
+
+#type() => mostrara que tipo de variable es
+print(type(year))
+print(type(descripcion))
+
+#python no se puede crear una variable sin un contenido
+#en python none = null | undefined
+especialidad = None
+
+print(type(especialidad))
+
+#
