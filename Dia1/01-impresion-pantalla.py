c1b8634b50e3d17cd49d666cf5e314775d3d886f
Juan Diego <jdalbinoramos@gmail.com>
2022-03-01 19:16:38 -0500

    semana1 actualizado

--- Dia1/01-impresion-pantalla.py
+++
@@ -1,8 +0,0 @@
-nombre = 'eduardo'
-print(nombre)
-#concatenar:
-print('el nombre es:',nombre,'del usuario')
-
-estado_civil = 'viudo'
-
-#si colocamos..
--- Dia1/01-variables.py
+++ Dia1/01-variables.py
@@ -1,32 +1,55 @@
-#Esto es un comentario y sirve para dar contexto de que se hace, se hizo o se hará.
-#TODO: logica para este controlador.
+# Esto es un comentario y sirve para dar contexto de que se hace, se hizo o se hara
+# TODO: logica para este controlador
 
-nombre = 'eduardo'
 edad = 30
 
-#variables de texto
+# variables de texto
 nombre = 'eduardo'
-apellido = "de'lala"
+apellido = "de'rivero"
+
+# si queremos tener un texto que pueda contener saltos de linea
 descripcion = """hola amigos:
-como estan?"""
+como estan?
+yo muy bien jeje
+"""
 
-descripcion2 = '''Hola amigos:
-what is up?'''
+descripcion2 = '''¿hola amigos:
+como estan?'''
 
-print(nombre)
+desCripcion2 = "adios"
 
-#variables numericas
+print(descripcion2)
+print(desCripcion2)
 
+# variables numericas
 year = 2022
 
-#type() => mostrara que tipo de variable es
+# type() => mostrara que tipo de variable es
 print(type(year))
-print(type(descripcion))
+print(type(desCripcion2))
 
-#python no se puede crear una variable sin un contenido
-#en python none = null | undefined
+# Python no se puede crear una variable sin un contenido a exepcion del None
+# en python None = null | undefined
 especialidad = None
+
+# en Python no hace validacion del tipo de dato primario (si la variable 'nace' siendo string) normal se puede cambiar su tipo a otro (Boolean, int, float, array, etc.)
+# en Python no existen las constantes
+dni= [123123123]
+dni= 'peruano'
+dni= False
+
+# id() > dara la ubicacion de esa variablen en relacion a la memoria del dispositivo
+print(id(dni))
+
 
 print(type(especialidad))
 
-#
+mes, dia = "febrero", 28
+
+print(mes)
+
+# del > elimina la variable de la memoria 
+del mes
+
+# si queremos usar luego de la eliminacion esa variable no sera posible ya que se elimino de la memoria
+print(mes)
