78f020f179842ff36b90f0403a0cbf844825ed43
Juan Diego <jdalbinoramos@gmail.com>
2022-03-24 19:57:25 -0500

    limpieza de la rama

--- project/models/ingredientes.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import Column,types
-from config import conexion
-class Ingrediente(conexion.Model):
-    id = Column(type_=types.Integer,primary_key=True,autoincrement=True)
-    nombre = Column(type_=types.String(45),nullable=False,unique=True)
-
-    __tablename__ = 'ingredientes'
