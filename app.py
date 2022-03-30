5284be8ee84c17726e6c281137c4671fceb7f294
Juan Diego <jdalbinoramos@gmail.com>
2022-03-30 17:06:10 -0500

    inicio jwt

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from os import environ
+from re import A
 from flask import Flask, app, render_template
 from flask_restful import Api
 from controllers.usuarios import LoginController, RegistroController
 from config import validador,conexion
 from dotenv import load_dotenv
+from flask_cors import CORS
+from flask_jwt import JWT
+from seguridad import autenticador,indentificador
 
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app=app)
+app.config['SECRET_KEY'] = "secreto"
 app.config["SQLALCHEMY_DATABASE_URI"]= environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
+
+jsonwebtoken = JWT(app=app,authentication_handler=autenticador,identity_handler=indentificador)
+
 
 api = Api(app=app)
 validador.init_app(app)
