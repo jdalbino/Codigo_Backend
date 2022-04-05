0b9b51c616b8924df4c19164940641408d960dec
Juan Diego <jdalbinoramos@gmail.com>
2022-04-04 19:59:28 -0500

    limpieza

--- models/categorias.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column,types
-from config import conexion
-
-class Categoria(conexion.Model):
-    __tablename__="categorias"
-
-    id = Column(type_=types.Integer,primary_key=True,autoincrement=True)
-    nombre = Column(type_=types.String(length=45),nullable=False)
