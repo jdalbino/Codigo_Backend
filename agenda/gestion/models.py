8b46bd628473e947a4a479e022b76ef6229b7b05
Juan Diego <jdalbinoramos@gmail.com>
2022-04-07 21:54:32 -0500

    cambios 2 migraciones

--- agenda/gestion/models.py
+++ agenda/gestion/models.py
@@ -1,4 +1,3 @@
-from curses.ascii import NUL
 from pyexpat import model
 from django.db import models
 
@@ -13,3 +12,25 @@ class Etiqueta(models.Model):
     class Meta:
         db_table = 'etiquetas'
         ordering = ['-nombre']
+
+class Tareas(models.Model):
+    
+    class CategoriaOpciones(models.TextChoices):
+        TODO = 'TODO','TO_DO'
+        IN_PROGRESS = 'IP','IN_PROGRESS'
+        DONE='DONE','DONE'
+        CANCELLED = 'CANCELLED','CANCELLED'
+
+    id= models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=45,null=False)
+    categoria = models.CharField(max_length=45,choices=CategoriaOpciones.choices,default=CategoriaOpciones.TODO)
+    fechaCaducidad = models.DateTimeField(db_column='fecha_caducidad')
+    importancia = models.IntegerField(null=False)
+
+    createdAt = models.DateTimeField(auto_now_add=True,db_column='created_at')
+    updatedAt = models.DateTimeField(auto_now=True,db_column='updated_at')
+
+    etiquetas = models.ManyToManyField(to=Etiqueta,related_name='tareas')
+
+    class Meta:
+        db_table = 'tareas'
---
+++ agenda/gestion/urls.py
@@ -0,0 +1,7 @@
+# from django.urls import path
+# from .views import inicio
+
+# urlpatters = [
+#     path('/inicio',inicio)
+# ]
+
