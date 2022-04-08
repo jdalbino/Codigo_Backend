31ceac4e83498837e50a9db22c4d9ffcf3ad723c
Juan Diego <jdalbinoramos@gmail.com>
2022-04-07 22:35:03 -0500

    serializers

--- agenda/gestion/urls.py
+++ agenda/gestion/urls.py
@@ -1,7 +1,7 @@
-# from django.urls import path
-# from .views import inicio
+from django.urls import path
+from .views import inicio
 
-# urlpatters = [
-#     path('/inicio',inicio)
-# ]
+urlpatterns = [
+     path('inicio',inicio)
+ ]
 
--- agenda/gestion/views.py
+++ agenda/gestion/views.py
@@ -1,8 +1,21 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.generics import ListAPIView
 
 @api_view(http_method_names=['GET','POST'])
-def inicio(request):
-    return Response(data={
-        "message":"Bienvenido a mi API de agenda"
-    })
+def inicio(request: Request):
+    print(request.method)
+    print(request)
+    if request.method == 'GET':
+           
+        return Response(data={
+            "message":"Bienvenido a mi API de agenda"
+        })
+    elif request.method == 'POST':
+        return Response(data={
+            "message":"Hice un post"
+        },status=201)
+
+class PruebaApiView(ListAPIView):
+    serializer = None
